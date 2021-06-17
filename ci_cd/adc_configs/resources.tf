5fa982cd039703c27c2598b5f2714fa21fe72497
Bharthi <bharthi31@mgail.co>
2021-06-17 18:02:24 +0530

    Optimised config file

--- ci_cd/adc_configs/resources.tf
+++ ci_cd/adc_configs/resources.tf
@@ -9,14 +9,14 @@ resource "citrixadc_csvserver" "demo_csvserver" {
 
 resource "citrixadc_lbvserver"  "lbvs"{
   ipv46       = var.lb_ip
-  name        = var.lbvs_name
+  name        = format("%s-lbvs", var.resource_prefix)
   port        = 80
   servicetype = "HTTP"
 }
 
 resource "citrixadc_service"  "service"{
     lbvserver = citrixadc_lbvserver.lbvs.name
-    name = var.backend_service_name
+    name = format("%s-service", var.resource_prefix)
     ip = var.backend_service_ip
     servicetype  = "HTTP"
     port = 80
@@ -28,7 +28,7 @@ resource "citrixadc_service"  "service"{
 resource "citrixadc_cspolicy" "cspolicy"{
   csvserver       = citrixadc_csvserver.demo_csvserver.name
   targetlbvserver = citrixadc_lbvserver.lbvs.name
-  policyname      = var.cspolicy_name
+  policyname      = format("$s-cspolicy", var.resource_prefix)
   rule            = format("HTTP.REQ.HOSTNAME.SERVER.EQ(\"demo-bg.webapp.com\") && HTTP.REQ.URL.PATH.SET_TEXT_MODE(IGNORECASE).STARTSWITH(\"/\") && sys.random.mul(100).lt(%s)", var.traffic_split_percentage)
   priority        = var.priority
 
--- ci_cd/adc_configs/variables.tf
+++ ci_cd/adc_configs/variables.tf
@@ -8,25 +8,15 @@ variable "backend_service_ip" {
   type        = string
   default     = "20.0.0.7"
 }
-variable "lbvs_name" {
-  description = "Name of LB Vserver"
+variable "resource_prefix" {
+  description = "Prefix of resources to be created"
   type = string
-  default = "lbvs"
+  default = "demo"
 }
 variable "lb_ip" {
   description = "IP of LB Vserver"
   type = string
   default = "1.1.1.1"
-}
-variable "cspolicy_name" {
-  description = "Name of CS Policy"
-  type = string
-  default = "cspolicy"
-}
-variable "backend_service_name" {
-  description = "backend service name"
-  type = string
-  default = "service"
 }
 variable "priority" {
   description = "CS Policy Priority"
